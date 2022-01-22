45aeb4ca42066d4276a463b6498928b95d5bd9e4
Kyle Tower <kyle.tower@gmail.com>
2022-01-22 16:55:59 -0500

    refactoring with components folder

---
+++ components/Results.js
@@ -0,0 +1,12 @@
+const Results = ({ geniusData }) => {
+  return (
+    <div>
+      {geniusData &&
+        geniusData.response.hits.map((hit, idx) => (
+          <p key={idx}>{hit.result.full_title}</p>
+        ))}
+    </div>
+  );
+};
+
+export default Results;
